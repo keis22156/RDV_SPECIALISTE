e03cdd85bc41b6f0987c04658ded9708d8daa39d
Nicolas <nicolas.chaudemanche1@gmail.com>
2025-04-12 15:00:00 +0200

    ajout Role.java

--- src/modele/Role.java
+++ src/modele/Role.java
@@ -1,5 +1,18 @@
 package modele;
 
+/**
+ * Enumération des rôles possibles pour un utilisateur de l’application.
+ * <ul>
+ *   <li>{@link #ADMIN} – Administrateur</li>
+ *   <li>{@link #PATIENT} – Patient</li>
+ *   <li>{@link #SPECIALISTE} – Spécialiste</li>
+ * </ul>
+ */
 public enum Role {
-    ADMIN, PATIENT, SPECIALISTE
+    /** Administrateur, peut gérer utilisateurs et consulter tous les RDV. */
+    ADMIN,
+    /** Patient, peut prendre RDV, consulter son historique et modifier ses infos. */
+    PATIENT,
+    /** Spécialiste, peut gérer son agenda et consulter son historique de consultations. */
+    SPECIALISTE
 }
