53cdd85e594ed352970fdcd5eda0aea5cada6ab9
Keis <keisaissaoui2@gmail.com>
2025-04-14 19:00:00 +0200

    ajout HeaderAccueil.java

--- src/vue/Accueil/HeaderAccueil.java
+++ src/vue/Accueil/HeaderAccueil.java
@@ -1,3 +1,5 @@
+// HeaderAccueil.java affiche la bannière de la page d’accueil
+
 package vue.Accueil;
 
 import javax.swing.*;
@@ -5,19 +7,24 @@ import java.awt.*;
 
 public class HeaderAccueil extends JPanel {
     public HeaderAccueil() {
+        // layout en border et fond vert sombre
         setLayout(new BorderLayout());
         setBackground(new Color(26, 57, 45));
         setBorder(BorderFactory.createEmptyBorder(20, 30, 20, 30));
 
+        // création du logo à gauche
         JLabel logo = new JLabel();
-        ImageIcon logoIcon = new ImageIcon("/Users/keisaissaoui/TP JAVA 2025/RDVSPECIALISTE/logoaccueil.png"); // à adapter
+        ImageIcon logoIcon = new ImageIcon("/Users/keisaissaoui/Documents/RDVSPECIALISTE/src/images/logoaccueil.png"); // chemin à adapter si besoin
+        // on redimensionne l’image en hauteur à 60 px
         Image image = logoIcon.getImage().getScaledInstance(-1, 60, Image.SCALE_SMOOTH);
         logo.setIcon(new ImageIcon(image));
 
+        // conteneur à gauche pour le logo
         JPanel blocGauche = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 0));
         blocGauche.setBackground(getBackground());
         blocGauche.add(logo);
 
+        // on ajoute le bloc gauche au panel principal
         add(blocGauche, BorderLayout.WEST);
     }
 }
